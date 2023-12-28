f3702e937530a4f7096f4f1e7b58488cf0f3802e
Gorsky Kirill <kirillgorskiy@gmail.com>
2023-12-28 22:45:59 +0300

    new: reset oclint to 22.02

--- build-oclint.sh
+++ build-oclint.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 if [ ! -d oclint ]; then
-    git clone https://github.com/oclint/oclint.git
+    git clone https://github.com/oclint/oclint.git oclint
+    cd oclint
+    git reset --hard d776db51c8574df406b2b0dc1b43b0b9b2d86d34
+    cd ..
 fi
 
 cd oclint/oclint-scripts
