30cb8b08841dc92a8e975099673e87a57cd02b65
Gorsky Kirill <kirillgorskiy@gmail.com>
2023-12-28 23:20:48 +0300

    new: change ex3 to c++

---
+++ examples/ex3/main.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int x = 0;
+    cin >> x;
+
+    if (x == 0) {
+        cout << "Zero";
+    }
+    else if (x == 1) {
+        cout << "One";
+    }
+    else if (x == 2) {
+        cout << "Two";
+    }
+    else if (x == 3) {
+        cout << "Three";
+    }
+    if (x == 4) {
+        cout << "Four";
+    }
+    else if (x == 5) {
+        cout << "Five";
+    }
+    if (x == 6) {
+        cout << "Six";
+    }
+    if (x == 7) {
+        cout << "Seven";
+    }
+    else if (x == 8) {
+        cout << "Eight";
+    }
+    else if (x == 9) {
+        cout << "Nine";
+    }
+    else {
+        cout << "Something else";
+    }
+}
