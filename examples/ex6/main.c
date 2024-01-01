efe2f3c7f32b04b7b75aace9bbc052bbf20afac3
Gorsky Kirill <kirillgorskiy@gmail.com>
2024-01-01 05:00:57 +0300

    rule: TooComplexCondition

---
+++ examples/ex6/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int a, b, c, d, e, f;
+    scanf("%d %d %d %d %d %d", &a, &b, &c, &d, &e, &f);
+
+    if (((a == b || b == c) && (d == e || e == f) || !(d == c)) && b != e) {
+        puts("Congrats! You found the secret code.");
+    }
+    else {
+        puts("Better luck next time.");
+    }
+
+    return 0;
+}
