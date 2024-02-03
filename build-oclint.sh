867c5140526326d7f9700120f51ad2ed016640c3
Gorsky Kirill <kirillgorskiy@gmail.com>
2024-02-03 03:58:20 +0300

    add commit hash variable

--- build-oclint.sh
+++ build-oclint.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+OCLINT_22_02_COMMIT=d776db51c8574df406b2b0dc1b43b0b9b2d86d34
+
 if [ ! -d oclint ]; then
     git clone https://github.com/oclint/oclint.git oclint
     cd oclint
-    git reset --hard d776db51c8574df406b2b0dc1b43b0b9b2d86d34
+    git reset --hard $OCLINT_22_02_COMMIT
     cd ..
 fi
 
