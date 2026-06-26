d724cffe1a3edf2b4141d61fb7f7e3368ccc89f9
Dmitry Ivanov <ivanovd.v3.0@gmail.com>
2026-06-26 18:34:26 +0300

    Update get_oclint.sh

--- scripts/get_oclint.sh
+++ scripts/get_oclint.sh
@@ -9,7 +9,7 @@ OCLINT_TAG=v26.02
 git clone --single-branch --depth 1 --branch $OCLINT_TAG https://github.com/oclint/oclint.git oclint
 
 cd oclint/oclint-scripts
-./makeWithSystemLLVM $(llvm-config-21 --libdir)
+./makeWithSystemLLVM /usr/lib/llvm-21/
 
 cd ..
 
