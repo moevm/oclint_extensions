b64160ae01985cc0e8c4970fa896202dfb174987
Dmitry Ivanov <darcenrall@gmail.com>
2026-06-27 18:45:11 +0300

    -  Update get_oclint.sh     -  copy results to /usr/local -  Update CMakeLists.txt     - CMAKE_CXX_STANDARD 14 -> 17     - use llvm (ver. 21) / oclint (26.02) / etc from /usr/local

--- scripts/get_oclint.sh
+++ scripts/get_oclint.sh
@@ -9,8 +9,17 @@ OCLINT_TAG=v26.02
 git clone --single-branch --depth 1 --branch $OCLINT_TAG https://github.com/oclint/oclint.git oclint
 
 cd oclint/oclint-scripts
-./make
+./makeWithSystemLLVM /usr/lib/llvm-21/
 
 cd ..
+
 cp -r build/oclint-release/lib/* /usr/local/lib/
 cp -r build/oclint-release/bin/* /usr/local/bin/
+
+cp -r build/oclint-core/lib/* /usr/local/lib/
+cp -r build/oclint-metrics/lib/* /usr/local/lib/
+cp -r build/oclint-rules/lib/* /usr/local/lib/
+
+cp -r oclint-core/include/* /usr/local/include/
+cp -r oclint-metrics/include/* /usr/local/include/
+cp -r oclint-rules/include/* /usr/local/include/
